d10dd4ebcd5b6c0e71271f1376f9fa846a6e2660
huytq <huytq000605@gmail.com>
2022-05-08 20:54:32 +0700

    Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,39 +3,45 @@ import { CustomProvider, FlexboxGrid, Container } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
 import { Sidebar } from "./Sidebar";
 import { MainContent } from "./MainContent";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
     return (
-        <div>
-            <CustomProvider theme="light">
-                <Container style={{style: "5px", backgroundColor: "#f2f2f5"}}>
-                    <img
-                        src={logo}
-                        alt="HUST Logo"
-                        style={{ height: "auto", width: "477px", padding: "5px" }}
-                    />
-									</Container>
-                <FlexboxGrid>
-                    <FlexboxGrid.Item
-                        colspan={4}
-                        style={{ height: "calc(100vh - 90px)" }}
-                    >
-                        <Container
-                            className="sidebar-page"
-                            style={{ height: "100%" }}
-                        >
-                            <Sidebar />
-                        </Container>
-                    </FlexboxGrid.Item>
-                    <FlexboxGrid.Item colspan={20}>
-                        <Container>
-                            <MainContent />
-                        </Container>
-                    </FlexboxGrid.Item>
-                </FlexboxGrid>
-            </CustomProvider>
-        </div>
-    );
+			<div>
+				<CustomProvider theme="light">
+					<Container style={{ style: "5px", backgroundColor: "#f2f2f5" }}>
+						<img
+							src={logo}
+							alt="HUST Logo"
+							style={{ height: "auto", width: "477px", padding: "5px" }}
+						/>
+					</Container>
+					<FlexboxGrid>
+						<FlexboxGrid.Item
+							colspan={4}
+							style={{ height: "calc(100vh - 90px)" }}
+						>
+							<Container className="sidebar-page" style={{ height: "100%" }}>
+								<Sidebar />
+							</Container>
+						</FlexboxGrid.Item>
+						<FlexboxGrid.Item colspan={20}>
+							<Container>
+								<Routes>
+									<Route path="/" element={<MainContent />} />
+									<Route path="/home" element={() => {
+										return (
+											<h1>Hello</h1>
+										)
+									}} />
+									<Route path="/statistics" element={<MainContent />} />
+								</Routes>
+							</Container>
+						</FlexboxGrid.Item>
+					</FlexboxGrid>
+				</CustomProvider>
+			</div>
+		);
 }
 
 export default App;
--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -4,53 +4,73 @@ import DashboardIcon from "@rsuite/icons/Dashboard";
 import SingleSourceIcon from "@rsuite/icons/SingleSource";
 import OperatePeopleIcon from "@rsuite/icons/OperatePeople";
 import PageIcon from "@rsuite/icons/Page";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useCallback } from "react"
 
 export const Sidebar = () => {
-    return (
-        <Sidenav
-            defaultOpenKeys={["3", "4"]}
-            collapsible
-            activeKey="1"
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                height: "100%",
-            }}
-        >
-            <Sidenav.Body style={{ height: "100%" }}>
-                <Nav style={{ height: "100%" }}>
-                    <Nav.Item eventKey="1" icon={<PageIcon />}>
-                        Trang chủ
-                    </Nav.Item>
-                    <Nav.Item eventKey="2" icon={<DashboardIcon />}>
-                        Thông số
-                    </Nav.Item>
-                    <Dropdown
-                        eventKey="3"
-                        title="Nhà máy điện"
-                        icon={<SingleSourceIcon />}
-                    >
-                        {[
-                            "Đống Đa",
-                            "Hai Bà Trưng",
-                            "Ba Đình",
-                            "Hoàn Kiếm",
-                        ].map((name, idx) => {
-                            return (
-                                <Dropdown.Item
-                                    eventKey={`3-${idx + 1}`}
-                                    icon={<OperatePeopleIcon />}
-                                >
-                                    Nhà máy quận {`${name}`}
-                                </Dropdown.Item>
-                            );
-                        })}
-                    </Dropdown>
-                    <Nav.Item eventKey="4" icon={<GearIcon />}>
-                        Cài đặt
-                    </Nav.Item>
-                </Nav>
-            </Sidenav.Body>
-        </Sidenav>
-    );
+	const navigate = useNavigate();
+	// const handleLink = () => {}
+	const handleLink = useCallback(
+		(link) => {
+			navigate(`/${link}`, { replace: true });
+		},
+		[navigate]
+	);
+
+	return (
+		<Sidenav
+			defaultOpenKeys={["3", "4"]}
+			collapsible
+			activeKey="1"
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				height: "100%",
+			}}
+		>
+			<Sidenav.Body style={{ height: "100%" }}>
+				<Nav style={{ height: "100%" }}>
+					<Nav.Item
+						eventKey="1"
+						icon={<PageIcon />}
+						onClick={() => {
+							handleLink("home");
+						}}
+					>
+						Trang chủ
+					</Nav.Item>
+					<Nav.Item
+						eventKey="2"
+						icon={<DashboardIcon />}
+						onClick={() => {
+							handleLink("statistics");
+						}}
+					>
+						Thông số
+					</Nav.Item>
+					<Dropdown
+						eventKey="3"
+						title="Nhà máy điện"
+						icon={<SingleSourceIcon />}
+					>
+						{["Đống Đa", "Hai Bà Trưng", "Ba Đình", "Hoàn Kiếm"].map(
+							(name, idx) => {
+								return (
+									<Dropdown.Item
+										eventKey={`3-${idx + 1}`}
+										icon={<OperatePeopleIcon />}
+									>
+										Nhà máy quận {`${name}`}
+									</Dropdown.Item>
+								);
+							}
+						)}
+					</Dropdown>
+					<Nav.Item eventKey="4" icon={<GearIcon />}>
+						Cài đặt
+					</Nav.Item>
+				</Nav>
+			</Sidenav.Body>
+		</Sidenav>
+	);
 };
