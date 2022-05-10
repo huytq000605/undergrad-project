01f08c87c9d9bf5288bcbe910cd63bc90bd8696a
huytq <huytq000605@gmail.com>
2022-05-10 23:34:13 +0700

    Format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,61 @@
-
 import { CustomProvider, FlexboxGrid, Container } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
 import { Sidebar } from "./Sidebar";
 import { Statistics } from "./Statistics";
 import { Routes, Route, Link } from "react-router-dom";
 import { Graph } from "./Graph";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
-import { useRef, useEffect, useCallback, useState } from "react"
+import { useEffect, useCallback, useState } from "react";
 
 function App() {
 	const onContentRefChange = useCallback((ref) => {
-		setContentRef(ref)
-	}, [])
+		setContentRef(ref);
+	}, []);
 	const onSidebarRefChange = useCallback((ref) => {
-		setSidebarRef(ref)
-	}, [])
-	const [contentRef, setContentRef] = useState(null)
-	const [sidebarRef, setSidebarRef] = useState(null)
+		setSidebarRef(ref);
+	}, []);
+	const [contentRef, setContentRef] = useState(null);
+	const [sidebarRef, setSidebarRef] = useState(null);
 
 	useEffect(() => {
-		if(contentRef && sidebarRef) {
-			sidebarRef.style.height = `${contentRef.clientHeight}px`
-			sidebarRef.style.minHeight = `calc(100vh - 90px)`
+		if (contentRef && sidebarRef) {
+			sidebarRef.style.height = `${contentRef.clientHeight}px`;
+			sidebarRef.style.minHeight = `calc(100vh - 90px)`;
 		}
-		// sidebarRef.current.height = contentRef.
-	}, [contentRef, sidebarRef])
+	}, [contentRef, sidebarRef]);
 
-	useEffect(() => {
-
-	}, [])
-    return (
-			<div>
-				<CustomProvider theme="light">
-					<Header />
-					<FlexboxGrid ref={onContentRefChange}>
-						<FlexboxGrid.Item
-							ref={onSidebarRefChange}
-							colspan={4}
-							style={{ display: "flex" }}
+	useEffect(() => {}, []);
+	return (
+		<div>
+			<CustomProvider theme="light">
+				<Header />
+				<FlexboxGrid ref={onContentRefChange}>
+					<FlexboxGrid.Item
+						ref={onSidebarRefChange}
+						colspan={4}
+						style={{ display: "flex" }}
+					>
+						<Container
+							className="sidebar-page"
+							style={{ display: "flex", height: "100%" }}
 						>
-							<Container className="sidebar-page" style={{ display: "flex", height: "100%" }}>
-								<Sidebar />
-							</Container>
-						</FlexboxGrid.Item>
-						<FlexboxGrid.Item colspan={20}>
-							<Container>
-								<Routes>
-									<Route path="/" element={<Statistics />} />
-									<Route path="/statistics" element={<Statistics />} />
-									<Route path="/graph" element={<Graph />} />
-								</Routes>
-							</Container>
-						</FlexboxGrid.Item>
-					</FlexboxGrid>
-				</CustomProvider>
-			</div>
-		);
+							<Sidebar />
+						</Container>
+					</FlexboxGrid.Item>
+					<FlexboxGrid.Item colspan={20}>
+						<Container>
+							<Routes>
+								<Route path="/" element={<Statistics />} />
+								<Route path="/statistics" element={<Statistics />} />
+								<Route path="/graph" element={<Graph />} />
+							</Routes>
+						</Container>
+					</FlexboxGrid.Item>
+				</FlexboxGrid>
+			</CustomProvider>
+		</div>
+	);
 }
 
 export default App;
