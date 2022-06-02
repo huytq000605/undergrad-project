1514741ec76d2f3ad4c6518b392a23e1b6678758
huytq <huytq000605@gmail.com>
2022-06-02 23:46:16 +0700

    Lint

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import { mqttClient, receiveTopic, sendTopic } from "./mqtt.js";
 import client from "knex";
 import cors from "cors";
 
 const app = express();
-const ENV = process.env
+const ENV = process.env;
 const port = 4000;
 
 export const knex = client({
@@ -23,7 +23,7 @@ export const knex = client({
 });
 
 // Write migration
-await knex("chi_so_pha_a").select(["dong_dien", "dien_ap"])
+await knex("chi_so_pha_a").select(["dong_dien", "dien_ap"]);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -31,66 +31,74 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.get('/pha-a', async (req, res) => {
-    const data = await knex("chi_so_pha_a").orderBy("created_at", "desc").first();
+app.get("/pha-a", async (req, res) => {
+    const data = await knex("chi_so_pha_a")
+        .orderBy("created_at", "desc")
+        .first();
     res.end(JSON.stringify(data));
-})
+});
 
-app.get('/pha-b', async (req, res) => {
-    const data = await knex("chi_so_pha_b").orderBy("created_at", "desc").first();
+app.get("/pha-b", async (req, res) => {
+    const data = await knex("chi_so_pha_b")
+        .orderBy("created_at", "desc")
+        .first();
     res.end(JSON.stringify(data));
-})
+});
 
-app.get('/pha-c', async (req, res) => {
-    const data = await knex("chi_so_pha_c").orderBy("created_at", "desc").first();
+app.get("/pha-c", async (req, res) => {
+    const data = await knex("chi_so_pha_c")
+        .orderBy("created_at", "desc")
+        .first();
     res.end(JSON.stringify(data));
-})
+});
 
-app.get('/3-pha', async (req, res) => {
+app.get("/3-pha", async (req, res) => {
     const [pha_a, pha_b, pha_c] = await Promise.all([
-         knex("chi_so_pha_a").orderBy("created_at", "desc").first(),
-         knex("chi_so_pha_b").orderBy("created_at", "desc").first(),
-         knex("chi_so_pha_c").orderBy("created_at", "desc").first()
-    ])
-    res.end(JSON.stringify({
-        pha_a: pha_a.dien_ap,
-        pha_b: pha_b.dien_ap,
-        pha_c: pha_c.dien_ap,
-        tan_so: "50"
-    }))
-})
+        knex("chi_so_pha_a").orderBy("created_at", "desc").first(),
+        knex("chi_so_pha_b").orderBy("created_at", "desc").first(),
+        knex("chi_so_pha_c").orderBy("created_at", "desc").first(),
+    ]);
+    res.end(
+        JSON.stringify({
+            pha_a: pha_a.dien_ap,
+            pha_b: pha_b.dien_ap,
+            pha_c: pha_c.dien_ap,
+            tan_so: "50",
+        })
+    );
+});
 
-app.get('/3-pha/graph', async (req, res) => {
+app.get("/3-pha/graph", async (req, res) => {
     const [pha_a, pha_b, pha_c] = await Promise.all([
         knex("chi_so_pha_a").orderBy("id", "desc").limit(30),
         knex("chi_so_pha_b").orderBy("id", "desc").limit(30),
-        knex("chi_so_pha_c").orderBy("id", "desc").limit (30)
-   ])
-   pha_a.reverse();
-   pha_b.reverse();
-   pha_c.reverse();
-   return res.json({
-       pha_a: {
-           dong_dien: pha_a.map((element) => element.dong_dien),
-           dien_ap: pha_a.map((element) => element.dien_ap),
-           time: pha_a.map((element) => element.created_at),
-       },
-       pha_b: {
-           dong_dien: pha_b.map((element) => element.dong_dien),
-           dien_ap: pha_b.map((element) => element.dien_ap),
-           time: pha_b.map((element) => element.created_at),
-       },
-       pha_c: {
-           dong_dien: pha_c.map((element) => element.dong_dien),
-           dien_ap: pha_c.map((element) => element.dien_ap),
-           time: pha_c.map((element) => element.created_at),
-       },
-       nhiet_do: new Array(30).fill(60) // fake data
-   })
-})
+        knex("chi_so_pha_c").orderBy("id", "desc").limit(30),
+    ]);
+    pha_a.reverse();
+    pha_b.reverse();
+    pha_c.reverse();
+    return res.json({
+        pha_a: {
+            dong_dien: pha_a.map((element) => element.dong_dien),
+            dien_ap: pha_a.map((element) => element.dien_ap),
+            time: pha_a.map((element) => element.created_at),
+        },
+        pha_b: {
+            dong_dien: pha_b.map((element) => element.dong_dien),
+            dien_ap: pha_b.map((element) => element.dien_ap),
+            time: pha_b.map((element) => element.created_at),
+        },
+        pha_c: {
+            dong_dien: pha_c.map((element) => element.dong_dien),
+            dien_ap: pha_c.map((element) => element.dien_ap),
+            time: pha_c.map((element) => element.created_at),
+        },
+        nhiet_do: new Array(30).fill(60), // fake data
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
-export default app
+export default app;
